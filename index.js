17f8cac187a74cc4aea29af59046369cf47d3812
timster788 <timster78811@gmail.com>
2018-03-27 20:59:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const driver = {};
  function updateDriverWithKeyAndValue(driver, key, value){
   return Object.assign({}, driver, {[key]: value})
 }
+  function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign(driver, {[key]: value})
+}
